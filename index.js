b0b846e7578a283af125fb8b2757b9b3a59eb49f
Young Jun Do <youngjundoe@gmail.com>
2021-09-07 16:23:38 -0400

    commit tree bea79196399ab39ddad89e620b1ce93476ba14b0

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+let num1 = 1;
+let num2 = 62;
 
-// Write your code here
+function multiplynumbers(){
+    //let num1 = 1;
+    //let num2 = 62;
+    return num1*num2;
+}
+
+let multiply = multiplynumbers();
+
+function randomGenerator(){
+    let returnVal = Math.ceil(Math.random()*10)+1;
+    return returnVal;
+}
+
+let random = randomGenerator();
+
+let num3 = 9;
+let num4 = 5;
+function modNumber(){
+    return num3%num4;
+}
+let mod = modNumber();
+
+function maxInSet(){
+    return Math.max(1,2,3,4,20);
+
+}
+
+let max = maxInSet();
